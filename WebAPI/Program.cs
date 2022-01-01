12be163dec595174887bc7eb6081f6e1b1f2843f
piotrwita <piotr.witaszewski@gmai.com>
2022-01-01 21:58:22 +0100

    Poprawa projektu WebAPI

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -9,13 +9,16 @@ builder.Services.InstallServicesInAssembly(builder.Configuration);
 
 var app = builder.Build();
 
-// configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.
 
 app.UseDeveloperExceptionPage();
 
-app.UseSwagger();
 
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();// (c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+}
 
 app.UseHttpsRedirection();
 
